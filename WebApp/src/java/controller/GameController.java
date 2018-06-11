9f10e051f91a3a0f7bac24f429f46700b0ab1a51
Salembere <ursula.salembere@gmail.com>
2018-06-11 23:19:39 +0200

    Page d'accueil : GameController final

--- WebApp/src/java/controller/GameController.java
+++ WebApp/src/java/controller/GameController.java
@@ -1,52 +1,10 @@
-
-/*package controller;
-import DAO.UserDAOImpl;
-import DAO.GameDAOImpl;
-import java.io.IOException;
-import java.util.ArrayList;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import model.Game;
-import model.User;
-
-
-  @author ursul
- 
-
-@WebServlet(name = "GameController", urlPatterns = {"/GameController"})
-public class GameController extends HttpServlet {
-
-
-     
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-        UserDAOImpl userDAO = new UserDAOImpl();
-        int idUser = Integer.parseInt(request.getParameter("idUser"));
-        int nbRound = Integer.parseInt(request.getParameter("nbRound"));
-        User user = userDAO.findById(idUser,new User());
-        
-                Game gameForm = new Game(nbRound, user);
-                GameDAOImpl gameDAO = new GameDAOImpl();
-                Game game = gameDAO.create(gameForm);
-        
-                System.out.println("test");
-                getServletConfig().getServletContext().getRequestDispatcher(
-                        "/vue/jeu.jsp").forward(request, response);
-            }
-    }*/
-
- 
-
 package controller;
 
 import DAO.GameDAOImpl;
 import DAO.UserDAOImpl;
 import java.io.IOException;
 import java.util.ArrayList;
+import static javassist.CtMethod.ConstParameter.string;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -55,7 +13,8 @@ import javax.servlet.http.HttpServletResponse;
 import model.Game;
 import model.User;
 
-
+ /*@author ursul*/
+         
 @WebServlet(name = "GameController", urlPatterns = {"/GameController"})
 public class GameController extends HttpServlet {
 
@@ -71,10 +30,9 @@ public class GameController extends HttpServlet {
                 Game gameForm = new Game(nbRound, user);
                 GameDAOImpl gameDAO = new GameDAOImpl();
                 Game game = gameDAO.create(gameForm);
-
-                /*request.setAttribute("pseudo", user.getPseudo());
+                
                 getServletConfig().getServletContext().getRequestDispatcher(
-                        "/vue/jeu.jsp").forward(request, response);*/
+                        "/vue/jeu.jsp?idUser="+idUser+"&nbRound="+nbRound).forward(request, response);
             }
 
 
